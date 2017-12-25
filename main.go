ffcd5c5cc88d8fc6b30ac76d5524a42d9c2662cb
gavinzhou <gavin.zhou@gmail.com>
2017-12-25 19:05:01 +0900

    first commit

--- main.go
+++ main.go
@@ -1,33 +1,12 @@
-/*
-Copyright 2017 The Kubernetes Authors.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
-// Note: the example only works with the code within the same release/branch.
 package main
 
 import (
-	"encoding/json"
-
-	"bufio"
 	"flag"
-	"fmt"
-	"os"
 	"path/filepath"
 
 	apiv1 "k8s.io/api/core/v1"
 	kerr "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
@@ -38,8 +17,8 @@ import (
 
 func main() {
 	s := map[string][]byte{
-		"admin":  []byte("admin"),
-		"passwd": []byte("passwd"),
+		"admin":    []byte("admin"),
+		"password": []byte("passwd"),
 	}
 
 	var kubeconfig *string
@@ -59,9 +38,9 @@ func main() {
 		panic(err)
 	}
 	secretName := "user-passwd"
-
+	pvcName := "test-pvc"
 	// deploymentsClient := clientset.AppsV1beta1().Deployments(apiv1.NamespaceDefault)
-	cur, err := clientset.CoreV1().Secrets(apiv1.NamespaceDefault).Get(secretName, metav1.GetOptions{})
+	_, err = clientset.CoreV1().Secrets(apiv1.NamespaceDefault).Get(secretName, metav1.GetOptions{})
 	if kerr.IsNotFound(err) {
 		secret := &apiv1.Secret{
 			ObjectMeta: metav1.ObjectMeta{
@@ -70,32 +49,30 @@ func main() {
 			},
 			Data: s,
 		}
-
 		clientset.CoreV1().Secrets(apiv1.NamespaceDefault).Create(secret)
+		// clientset.CoreV1().Secrets(apiv1.NamespaceDefault).Update(secret)
 	}
-
-	curJson, err := json.Marsha1(cur.DeepCopy())
-	fmt.Printf("Get secret %s.\n", curJson)
-
-	// Create Deployment
-	fmt.Println("Geting secret...")
-	// result, err := deploymentsClient.Create(deployment)
-	if err != nil {
-		panic(err)
+	// pvcSpec.StorageClassName = ""
+	StorageClass := "standard"
+	pvc := &apiv1.PersistentVolumeClaim{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: pvcName,
+			Annotations: map[string]string{
+				"volume.beta.kubernetes.io/storage-class": "standard",
+			},
+		},
+		Spec: apiv1.PersistentVolumeClaimSpec{
+			VolumeName: "test-pvc",
+			AccessModes: []apiv1.PersistentVolumeAccessMode{
+				apiv1.ReadWriteOnce,
+			},
+			StorageClassName: &StorageClass,
+			Resources: apiv1.ResourceRequirements{
+				Requests: apiv1.ResourceList{
+					apiv1.ResourceStorage: resource.MustParse("1Gi"),
+				},
+			},
+		},
 	}
-	fmt.Printf("Get secret %s.\n", cur.Data)
+	clientset.CoreV1().PersistentVolumeClaims(apiv1.NamespaceDefault).Create(pvc)
 }
-
-func prompt() {
-	fmt.Printf("-> Press Return key to continue.")
-	scanner := bufio.NewScanner(os.Stdin)
-	for scanner.Scan() {
-		break
-	}
-	if err := scanner.Err(); err != nil {
-		panic(err)
-	}
-	fmt.Println()
-}
-
-func int32Ptr(i int32) *int32 { return &i }
