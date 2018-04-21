e9fef73565b1700d9b80784eaaa993e2a897e01f
gavinzhou <gavin.zhou@gmail.com>
2018-04-21 16:41:41 +0900

    tutorial 2

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"path/filepath"
@@ -8,6 +9,8 @@ import (
 	core "k8s.io/api/core/v1"
 	kerr "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/util/strategicpatch"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/homedir"
@@ -17,10 +20,8 @@ import (
 
 // CreateSecret is CreateSecret
 func CreateSecret(c kubernetes.Interface, meta metav1.ObjectMeta, transform func(*core.Secret) *core.Secret) (*core.Secret, string, error) {
-	cur, err := c.CoreV1().Secrets(core.NamespaceDefault).Get(meta.Name, metav1.GetOptions{})
-	fmt.Println(cur.Name)
+	cur, err := c.CoreV1().Secrets(meta.Namespace).Get(meta.Name, metav1.GetOptions{})
 	if kerr.IsNotFound(err) {
-		fmt.Printf("create secret %v", meta.Name)
 		out, err := c.CoreV1().Secrets(meta.Namespace).Create(transform(&core.Secret{
 			TypeMeta: metav1.TypeMeta{
 				Kind:       "Secret",
@@ -34,24 +35,37 @@ func CreateSecret(c kubernetes.Interface, meta metav1.ObjectMeta, transform func
 		// fmt.Printf("update secret %v", meta.Name)
 		// c.CoreV1().Secrets(core.NamespaceDefault).Update(secret)
 	}
-	return nil, "exist", nil
+	return PatchSecret(c, cur, transform)
+
 }
 
-// func PatchSecret(c kubernetes.Interface, cur *core.Secret, transform func(*core.Secret) *core.Secret) (*core.Secret, string, error) {
-// 	return PatchSecretObject(c, cur, transform(cur.DeepCopy()))
-// }
+func PatchSecret(c kubernetes.Interface, cur *core.Secret, transform func(*core.Secret) *core.Secret) (*core.Secret, string, error) {
+	return PatchSecretObject(c, cur, transform(cur.DeepCopy()))
+}
 
-// func PatchSecretObject(c kubernetes.Interface, cur, mod *core.Secret) {
-// 	curJson, err := json.Marshal(cur)
-// 	if err != nil {
-// 		return
-// 	}
+func PatchSecretObject(c kubernetes.Interface, cur, mod *core.Secret) (*core.Secret, string, error) {
+	curJson, err := json.Marshal(cur)
+	if err != nil {
+		return nil, "cur-unchanged", err
+	}
 
-// 	modJson, err := json.Marshal(mod)
-// 	if err != nil {
-// 		return
-// 	}
-// }
+	modJson, err := json.Marshal(mod)
+	if err != nil {
+		return nil, "mod-unchanged", err
+	}
+
+	patch, err := strategicpatch.CreateTwoWayMergePatch(curJson, modJson, core.Secret{})
+	if err != nil {
+		return nil, "patch-unchanged", err
+	}
+
+	if len(patch) == 0 || string(patch) == "{}" {
+		return cur, "unchanged", nil
+	}
+	fmt.Printf("Patching Secret %s/%s", cur.Namespace, cur.Name)
+	out, err := c.CoreV1().Secrets(cur.Namespace).Patch(cur.Name, types.StrategicMergePatchType, patch)
+	return out, "Patched", err
+}
 
 func main() {
 
@@ -72,17 +86,33 @@ func main() {
 		panic(err)
 	}
 	secretName := "user-passwd"
-	s := map[string][]byte{
-		"admin":    []byte("admin"),
-		"password": []byte("passwd"),
+	data := map[string][]byte{
+		"admin":  []byte("admin"),
+		"passwd": []byte("passwd"),
 	}
-	_, status, _ := CreateSecret(clientset,
+	_, status, err := CreateSecret(clientset,
 		metav1.ObjectMeta{Name: secretName, Namespace: core.NamespaceDefault},
 		func(in *core.Secret) *core.Secret {
 			if in.Data == nil {
-				in.Data = s
+				in.Data = data // can not change data
 			}
+			// in.Data["admin"] = []byte("admin")
+			// in.Data["passwd"] = []byte("passwd")
+			// this OK
 			return in
 		})
 	fmt.Println(status)
+	fmt.Println(err)
+	// _, s, err := core_util.CreateOrPatchSecret(clientset,
+	// 	metav1.ObjectMeta{Namespace: core.NamespaceDefault, Name: secretName},
+	// 	func(in *core.Secret) *core.Secret {
+	// 		if in.Data == nil {
+	// 			in.Data = make(map[string][]byte)
+	// 		}
+	// 		in.Data["admin"] = []byte("admin")
+	// 		in.Data["password"] = []byte("password")
+	// 		return in
+	// 	})
+	// fmt.Println(s)
+	// fmt.Println(err)
 }
